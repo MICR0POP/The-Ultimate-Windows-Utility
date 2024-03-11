3f40e6b14936928c1d177f2809456e3297d2400e
David Clarke <136286357+MICR0POP@users.noreply.github.com>
2024-03-10 21:53:56 -0600

    Add files via upload

---
+++ scripts/start.ps1
@@ -0,0 +1,26 @@
+<#
+.NOTES
+    Author         : Chris Titus @christitustech
+    Runspace Author: @DeveloperDurp
+    GitHub         : https://github.com/ChrisTitusTech
+    Version        : #{replaceme}
+#>
+
+Start-Transcript $ENV:TEMP\Winutil.log -Append
+
+#Load DLLs
+Add-Type -AssemblyName System.Windows.Forms
+
+# variable to sync between runspaces
+$sync = [Hashtable]::Synchronized(@{})
+$sync.PSScriptRoot = $PSScriptRoot
+$sync.version = "#{replaceme}"
+$sync.configs = @{}
+$sync.ProcessRunning = $false
+
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://christitus.com/win | iex"
+    break
+}
